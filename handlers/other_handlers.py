763cd298ddadf5fc53eb348fe60bcb942c04a732
Vlad Khmarenko <vladkhmarenko@icloud.com>
2024-12-28 13:38:41 +0300

    routers for echobot

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,7 +1,10 @@
+from aiogram import Router
 from aiogram.types import Message
 from lexicon.lexicon import LEXICON_RU
 
-@dp.message()
+router = Router()
+
+@router.message()
 async def send_echo(message: Message):
     try:
         await message.send_copy(chat_id=message.chat.id)
