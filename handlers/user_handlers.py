95f68d9304cca9124c0f8872f60bc1a8de218cf7
Vlad Khmarenko <vladkhmarenko@icloud.com>
2024-12-28 17:40:00 +0300

    lexicon fix, add lexicon and handler for mode and padding

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -3,6 +3,7 @@ from aiogram.types import Message
 from aiogram.filters import Command
 from lexicon.lexicon import LEXICON_RU
 from data import Data
+from keyboards.keyboards import mode_keyboard, padding_keyboard
 
 router = Router()
 
@@ -15,3 +16,30 @@ async def process_start_command(message: Message):
 @router.message(Command(commands='help'))
 async def process_help_command(message: Message):
     await message.answer(text=LEXICON_RU['/help'])
+
+
+@router.message(Command(commands='password'))
+async def process_password_command(message: Message):
+    pass
+
+@router.message(Command(commands='mode'))
+async def process_mode_command(message: Message):
+    await message.answer(
+        text=LEXICON_RU['choose_mode'],
+        reply_markup=mode_keyboard
+    )
+
+@router.message(Command(commands='padding'))
+async def process_padding_command(message: Message):
+    await message.answer(
+        text=LEXICON_RU['choose_padding'],
+        reply_markup=padding_keyboard
+    )
+
+@router.message(Command(commands='encrypt'))
+async def process_encrypt_command(message: Message):
+    pass
+
+@router.message(Command(commands='decrypt'))
+async def process_decrypt_command(message: Message):
+    pass
